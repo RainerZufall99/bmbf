e41616bcc1e21e09e8462595a185ae9c5f6788bf
RainerZufall99 <98850768+RainerZufall99@users.noreply.github.com>
2023-11-15 13:15:33 +0100

    update 1

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -163,8 +163,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Fragen Sie mich etwas!</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Ich beantworte Ihnen alle Fragen zum Bundesministerium für Bildung und Forschung!</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
